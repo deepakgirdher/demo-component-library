fcc474bdf51268e980dba76d258f43fa38ebd115
unknown <deepakgirdher@gmail.com>
2023-09-13 10:29:31 +0530

    Published branch "main"

--- .storybook/adventureTheme.ts
+++
@@ -1,9 +0,0 @@
-import { create } from '@storybook/theming/create';
-
-export default create({
-  base: 'light',
-  brandTitle: 'Adventure UI',
-  brandUrl: 'https://example.com',
-  brandImage: 'https://storage.googleapis.com/prd-adventurekk-en/wp-content/uploads/f54cdbf7-logo_adventure.svg',
-  brandTarget: '_self',
-});
--- src/atoms/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as Label } from "./Label";
-export { default as Icon } from "./Icon";
-
